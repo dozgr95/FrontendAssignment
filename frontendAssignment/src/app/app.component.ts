034bc4eaf2234eaed4975186d83fc21ea3b5cc01
Doz <dominik.zgraggen@stud.hslu.ch>
2020-11-18 19:15:54 +0100

    implement component with notification list

--- frontendAssignment/src/app/app.component.ts
+++ frontendAssignment/src/app/app.component.ts
@@ -10,4 +10,21 @@ import { loadNotifications, notification } from './notificationsHelper';
 export class AppComponent {
   title = 'frontendAssignment';
   notifications: notification[] = loadNotifications();
+  readItem = '';
+
+  deleteNotification(deleteNotification: notification) {
+    const index = this.notifications.findIndex(notification => notification.title === deleteNotification.title)
+    this.notifications.splice(index, 1)
+  }
+
+  readNotification(notification: notification) {
+    if(this.readItem){
+      this.readItem = ''
+    }else{
+      this.readItem = notification.title
+    }
+    if (notification.status === "new") {
+      notification.status ="old"
+    }        
+  }
 }
